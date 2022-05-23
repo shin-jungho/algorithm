4cc62ea1a0fcbbd9bd82e676e02e6a5df67bf273
jungho shin <wjdghtls11@gmail.com>
2022-05-23 21:21:12 +0900

    Add: 시저암호

--- level 1/*시저 암호.js
+++ level 1/*시저 암호.js
@@ -1,9 +1,11 @@
-// 시저 암호 *사람들 코드 보고 혼자 다시풀기
+// 시저 암호 *사람들 코드 보고 이해
 // 아스키 코드 A~Z(65~90) , a~z(97~122)
 // 문자열 -> 아스키코드 : s.charCodeAt()
 // 아스키코드 -> 문자열 : String.fromCharCode()
 
 function solution(s, n) {
-  var answer = '';
-  return answer;
+  return s.split('').map(v => {
+      if(v === ' ') return v;
+      return v.toUpperCase().charCodeAt()+n > 90 ? String.fromCharCode(v.charCodeAt()+n-26) : String.fromCharCode(v.charCodeAt()+n)
+  }).join('');
 }
