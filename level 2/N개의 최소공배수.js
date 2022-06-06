6a7b766aef343e98e8dfebc09b5f6c2a5a2aca91
jungho shin <wjdghtls11@gmail.com>
2022-06-06 13:41:48 +0900

    update: N개의 최소공배수

--- level 2/N개의 최소공배수.js
+++ level 2/N개의 최소공배수.js
@@ -1,21 +1,55 @@
 // 유클리드 호제법 활용해서
-// 유클리드 호제법을 이용하여 최대공약수를 구하고 최소공배수는 두수의 곱 / 최대공약수 이므로 이를 이용하여 문제를 해결
+// 유클리드 호제법을 이용하여 최대공약수를 구하고 최소공배수는 (두수의 곱 / 최대공약수) 이므로 이를 이용하여 문제를 해결
 
-function gcd (a, b) {
-  if (a % b === 0) {
-    return b;
-  } else { 
-    return gcd(b, a % b);
-  }
+// 틀림
+// function gcd (a, b) {
+//   if (a % b === 0) {
+//     return b;
+//   } else { 
+//     return gcd(b, a % b);
+//   }
+// }
+// function solution(arr) {
+//   let a = arr[0];
+//   let b = 0;
+//   let lcm = 0;
+//   for (let i = 1; i < arr.length; i++) {
+//     b = arr[i];
+//     lcm = (a * b / gcd(a, b)> lcm ? (a * b / gcd(a, b)) : lcm
+//     a = lcm;
+//   }
+//   return lcm;
+// }
+
+// 2번째
+
+function gcd(a, b) {
+  if(a === 0) return b;
+  return gcd (b % a, a);
 }
+
+function lcm(a, b) {
+  return (a * b / gcd(a, b));
+}
+
 function solution(arr) {
-  let a = arr[0];
-  let b = 0;
-  let lcm = 0;
-  for (let i = 1; i < arr.length; i++) {
-    b = arr[i];
-    lcm = (a * b / gcd(a, b)> lcm ? (a * b / gcd(a, b)) : lcm
-    a = lcm;
+  let result = 1;
+  
+  for (let i = 0; i < arr.length; i++) {
+    result = lcm(result, arr[i]);
   }
-  return lcm;
+
+  return result;
 }
+
+// 다른 사람 코드
+
+// function solution(arr) {
+//   let gcd = (a, b) => a%b===0 ? b : gcd(b, a%b);
+//   return arr.reduce((a, b) => a * b / gcd(a, b));
+// }
+
+//* reduce함수를 통해 배열을 순차적으로 돌면서 계산해주면 최소공배수가 나옴
+let arr = [2, 6, 8, 14];
+
+console.log(solution(arr));
